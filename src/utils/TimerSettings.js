bb0bd51a80be3e31663552aba63a3ed7c6761fae
Kennedy Gitahi <kengitahi@hotmail.com>
2025-05-04 06:33:36 +0300

    Reverting

--- src/utils/TimerSettings.js
+++ src/utils/TimerSettings.js
@@ -1,7 +1,9 @@
 import { reactive } from 'vue';
 
+//Values in milliseconds
 export const timerSettings = reactive({
-  workDuration: 25 * 60 * 1000,      // 25 minutes
+  // workDuration: 25 * 60 * 1000,      // 25 minutes
+  workDuration: 5 * 1000,        // 5 seconds for testing
   shortBreakDuration: 5 * 60 * 1000, // 5 minutes
   longBreakDuration: 15 * 60 * 1000  // 15 minutes
 });
